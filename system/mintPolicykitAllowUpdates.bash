e8c916f6c4ae3281d6201b686baf2a7ae076bf01
jayshomebrew <58111409+jayshomebrew@users.noreply.github.com>
2026-07-12 10:22:09 -0700

    Comment out set -euo pipefail in mintPolicykitAllowUpdates.bash
    Commented out the 'set -euo pipefail' line for debugging purposes.

--- system/mintPolicykitAllowUpdates.bash
+++ system/mintPolicykitAllowUpdates.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
+##set -euo pipefail
 
 # download and run:
 # curl -fsSL https://raw.githubusercontent.com/jayshomebrew/bash-scripts/main/system/mintPolicykitAllowUpdates.bash | bash
